2c0e532556c88964267def4d17c27fdcde7df0db
Stefan <stefan16.nguyen@gmail.com>
2023-12-09 19:23:40 +0100

    New Change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,5 +28,6 @@ public class Main{
             System.out.println("Drone Carriage Type: " + droneCarriageType.get(x));
             System.out.println();
         }
+        System.out.println("Hello World");
     }
 }
