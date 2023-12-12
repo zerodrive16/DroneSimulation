32b30eb859783cfd5688ba20227f3d2340b825e0
stefannguyen <stefan16.nguyen@gmail.com>
2023-12-12 21:52:27 +0100

    This is a new Change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -47,6 +47,6 @@ public class Main{
         ArrayList<String> droneTypeMaxCarriage = droneTypeData.getDroneMaxCarriage();
 
         System.out.println("Drone Manufacturer: " + droneTypeManufacturer);
-        System.out.println("This is a new change");
+        System.out.println("New Change");
     }
 }
