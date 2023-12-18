e94859dd9934eb03937868595e313dd716229958
Felix Dallmeyer <felix.dallmeyer1@gmail.com>
2023-12-19 00:48:01 +0100

    anfang Dronedynamics

--- src/main/java/org/example/API_Properties/DroneDynamic.java
+++ src/main/java/org/example/API_Properties/DroneDynamic.java
@@ -1,4 +1,31 @@
 package org.example.API_Properties;
 
+import java.util.ArrayList;
+
 public class DroneDynamic {
+    private String data_droneURL;
+    private String data_droneTimestamp;
+    private String data_droneSpeed;
+    private String data_droneAlignRoll;
+    private String data_droneAlignPitch;
+    private String data_droneYaw;
+    private String data_droneLongitude;
+    private String data_droneLatitude;
+    private String data_droneBatteryStatus;
+    private String data_droneLastSeen;
+    private String data_droneStatus;
+
+   public String getDroneURL(){return data_droneURL;}
+   public String getDroneTimestamp(){return data_droneTimestamp;}
+   public String getDroneSpeed(){return data_droneSpeed;}
+   public String getDroneAlignRoll(){return data_droneAlignRoll;}
+   public String getDroneAlignPitch(){return data_droneAlignPitch;}
+   public String getDroneYaw(){return data_droneYaw;}
+   public String getDroneLongitude(){return data_droneLongitude;}
+   public String getDroneLatitude(){return data_droneLatitude;}
+   public String getDroneBatteryStatus(){return data_droneBatteryStatus;}
+   public String getDroneLastSeen(){return data_droneLastSeen;}
+   public String getDroneStatus(){return data_droneStatus;}
+
+
 }
