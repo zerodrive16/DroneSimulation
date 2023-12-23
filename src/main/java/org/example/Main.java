64ad0501ffb70156f70a5694b09d36cc65f8a256
Stefan <stefan16.nguyen@gmail.com>
2023-12-23 22:15:03 +0100

    Changes Stefan 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,53 +6,51 @@ import org.example.API_Endpoints.DroneTypes;
 import org.example.API_Endpoints.Drones;
 import org.example.GUI.GUI;
 
-import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
 
-public class Main{
-    public static void main(String[] args){
-         GUI gui = new GUI();
-        // creating new Drones and access the Data from the function
-        Drones dronesAPI = new Drones();
-        DronesData.ReturnDroneData droneData = dronesAPI.APIDrones();
+        // Fetching drones data
+        DronesData.ReturnDroneData droneData = new Drones().APIDrones();
+        printDroneData(droneData);
 
-        // creating variables that take the data from the encapsulation
-        ArrayList<String> droneId = droneData.getDroneID();
-        ArrayList<String> droneType = droneData.getDroneTypeURL();
-        ArrayList<String> droneCreate = droneData.getDroneCreate();
-        ArrayList<String> droneSerialnumber = droneData.getDroneSerialnumber();
-        ArrayList<String> droneCarriageWeight = droneData.getDroneCarriageWeight();
-        ArrayList<String> droneCarriageType = droneData.getDroneCarriageType();
+        // Fetching drone types data
+        DroneTypesData.ReturnDroneTypeData droneTypeData = new DroneTypes().APIDroneTypes();
+        printDroneTypeData(droneTypeData);
+    }
 
-        // output the data
-        for(int x = 0; x < droneId.size(); x++){
-            System.out.println("Drone ID: " + droneId.get(x));
-            System.out.println("DroneType: " + droneType.get(x));
-            System.out.println("Drone Creation: " + droneCreate.get(x));
-            System.out.println("Drone Serialnumber: " + droneSerialnumber.get(x));
-            System.out.println("Drone Carriage Weight: " + droneCarriageWeight.get(x));
-            System.out.println("Drone Carriage Type: " + droneCarriageType.get(x));
-            System.out.println();
+    private static void printDroneData(DronesData.ReturnDroneData data) {
+        if (data == null) {
+            System.out.println("No drone data available.");
+            return;
         }
 
-        DroneTypes droneTypeAPI = new DroneTypes();
-        DroneTypesData.ReturnDroneTypeData droneTypeData = droneTypeAPI.APIDroneTypes();
+        for (int i = 0; i < data.getDroneID().size(); i++) {
+            System.out.println("Drone ID: " + data.getDroneID().get(i));
+            System.out.println("DroneType: " + data.getDroneTypeURL().get(i));
+            System.out.println("Drone Creation: " + data.getDroneCreate().get(i));
+            System.out.println("Drone Serialnumber: " + data.getDroneSerialnumber().get(i));
+            System.out.println("Drone Carriage Weight: " + data.getDroneCarriageWeight().get(i));
+            System.out.println("Drone Carriage Type: " + data.getDroneCarriageType().get(i));
+            System.out.println();
+        }
+    }
 
-        ArrayList<String> droneTypeManufacturer = droneTypeData.getDroneManufacturer();
-        ArrayList<String> droneTypeTypeName = droneTypeData.getDroneTypeName();
-        ArrayList<String> droneTypeWeight = droneTypeData.getDroneWeight();
-        ArrayList<String> droneTypeMaxSpeed = droneTypeData.getDroneMaxSpeed();
-        ArrayList<String> droneTypeBatteryCapacity = droneTypeData.getDroneBatteryCapacity();
-        ArrayList<String> droneTypeControlRange = droneTypeData.getDroneControlRange();
-        ArrayList<String> droneTypeMaxCarriage = droneTypeData.getDroneMaxCarriage();
+    private static void printDroneTypeData(DroneTypesData.ReturnDroneTypeData data) {
+        if (data == null) {
+            System.out.println("No drone type data available.");
+            return;
+        }
 
-        for(int i = 0; i < droneId.size(); i++) {
-            System.out.println("Drone Manufacturer: " + droneTypeManufacturer.get(i));
-            System.out.println("Drone TypeName: " + droneTypeTypeName.get(i));
-            System.out.println("Drone TypeWeight: " + droneTypeWeight.get(i));
-            System.out.println("Drone MaxSpeed: " + droneTypeMaxSpeed.get(i));
-            System.out.println("Drone BatteryCapacity: " + droneTypeBatteryCapacity.get(i));
-            System.out.println("Drone Control Range: " + droneTypeControlRange.get(i));
-            System.out.println("Drone MaxCarriage: " + droneTypeMaxCarriage.get(i));
+        for (int i = 0; i < data.getDroneManufacturer().size(); i++) {
+            System.out.println("Drone Manufacturer: " + data.getDroneManufacturer().get(i));
+            System.out.println("Drone TypeName: " + data.getDroneTypeName().get(i));
+            System.out.println("Drone TypeWeight: " + data.getDroneWeight().get(i));
+            System.out.println("Drone MaxSpeed: " + data.getDroneMaxSpeed().get(i));
+            System.out.println("Drone BatteryCapacity: " + data.getDroneBatteryCapacity().get(i));
+            System.out.println("Drone Control Range: " + data.getDroneControlRange().get(i));
+            System.out.println("Drone MaxCarriage: " + data.getDroneMaxCarriage().get(i));
+            System.out.println();
         }
     }
 }
