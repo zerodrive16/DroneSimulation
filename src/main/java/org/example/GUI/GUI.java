a1d22684b1496e88f747ea5ab88e9816dbe86d1b
Stefan <110019272+zerodrive16@users.noreply.github.com>
2024-01-21 15:36:58 +0100

    Changes by bbl

--- src/main/java/org/example/GUI/GUI.java
+++ src/main/java/org/example/GUI/GUI.java
@@ -11,7 +11,7 @@ import java.time.*;
 import java.util.Objects;
 
 
-public class GUI extends Abs_GUIComponents implements ActionListener{
+public class GUI extends Abs_GUIComponents{
 JFrame frame = new JFrame();
 
     public GUI(){
@@ -26,18 +26,18 @@ JFrame frame = new JFrame();
         addingComponents();
     }
 
-
     private void mainScreenSettings() {
-
-        mainScreen.setPreferredSize(mainscreenSize);
+        //mainScreen.setPreferredSize(new Dimension(width,height-(height/12)));
         mainScreen.setLayout(cardLayout);
-        mainScreen.add(card1, "Card1");
         configureCard1(mainScreenColor, card1);
-
         card1.setBackground(mainScreenColor);
 
-    }
+        JScrollPane scrollPane = new JScrollPane(card1);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
+        mainScreen.add(scrollPane, "Dashboard");
+    }
 
     public void configureCard1(Color mainScreenColor, JPanel card1) {
         Dashboard card = new Dashboard();
@@ -46,27 +46,24 @@ JFrame frame = new JFrame();
 
     private void navbarSettings() {
         navbar.setBackground(backgroundColor);
-        navbar.setPreferredSize(navbarSize);
+        navbar.setPreferredSize(new Dimension(width,(height/12)));
+        JLabel dashboardLabel = new JLabel(" Dashboard");
+        navbar.add(dashboardLabel,BorderLayout.WEST);
 
-//----------------------------tabsPanel and the tabs-------------------------------
         JPanel tabsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         tabsPanel.setBackground(backgroundColor);
-        tabsPanel.setPreferredSize(new Dimension(2* navbarButtonSize.width,navbarButtonSize.height));
-
-        tab1.setPreferredSize(navbarButtonSize);
+        tabsPanel.setPreferredSize(new Dimension((width/4)-10,(height/18)-10));
         tab1.setBackground(mainScreenColor);
-
+        tab1.setPreferredSize(new Dimension((width/4)-10,(height/18)-10));
         tabsPanel.add(tab1);
-
         navbar.add(tabsPanel,BorderLayout.SOUTH);
 
-//----------------------------time and date-------------------------------
 
         JTextArea time = new JTextArea("HH:mm"+"      "+"dd.MM.yyyy");
-        quickSet(titleFont,Color.white,backgroundColor,time);
+        time.setEditable(false);
+        quickSet(titleFont,Color.white,backgroundColor,time,dashboardLabel);
 
         navbar.add(time,BorderLayout.EAST);
-
 
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
@@ -83,16 +80,12 @@ JFrame frame = new JFrame();
                     String outputDate = dateNow.format(dateFormat);
                     time.setText(outputTime+"     "+outputDate);
                 }
-            }
-        });
-
+            }}
+        );
         timeAndDate.start();
     }
 
-    //----------------------------info bar and refresh button-------------------------------
-    //The infobar has a refresh button to refresh the data
     private void infobarSettings() {
-        JLabel creatorText = new JLabel(">>this program was made by group15<< ");
         JLabel refreshText = new JLabel("since Last Update: X Seconds     ");
         Timer refreshTimer = new Timer(1000, new ActionListener() {
             int seconds = 0;
@@ -103,43 +96,31 @@ JFrame frame = new JFrame();
                 }
                 seconds++;
                 refreshText.setText("since Last Update: "+seconds+" Seconds     ");
-            }
-        });
+            }}
+        );
         refreshTimer.start();
 
         infobar.setLayout(new BoxLayout(infobar, BoxLayout.X_AXIS));
         infobar.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         infobar.setBackground(backgroundColor);
-        infobar.setPreferredSize(infobarSize);
-        refreshButton.setPreferredSize(new Dimension(infobarSize.width/16,infobarSize.height-8));
-        refreshButton.setBackground(Color.GREEN);
+        infobar.setPreferredSize(new Dimension(width,height/32));
+        refreshButton.setBackground(Color.WHITE);
+        refreshButton.setPreferredSize(new Dimension(width/16,(height/32)-8));
         refreshButton.setBorderPainted(false);
         refreshButton.setFocusPainted(false);
-
         refreshButton.addActionListener(e->{
             refreshTimer.restart();
             refreshText.setText("since Last Update: 0 Seconds     ");
-
             /*
-
-
-
             UPDATE FUNKTION
-
-
-
              */
-
         });
 
-        quickSet(new Font("Roboto",Font.PLAIN,12),Color.WHITE,backgroundColor,refreshText,creatorText);
-        infobar.add(Box.createRigidArea(new Dimension((width/5)*2,infobarSize.height)));
-        infobar.add(creatorText);
-        infobar.add(Box.createHorizontalGlue());
+        quickSet(textFont,Color.WHITE,backgroundColor,refreshText);
+        infobar.add(Box.createHorizontalGlue()); //align right side
         infobar.add(refreshText);
         infobar.add(refreshButton);
     }
-//----------------------------add components-------------------------------
 
     private void addingComponents() {
         frame.add(navbar, BorderLayout.NORTH);
@@ -147,7 +128,6 @@ JFrame frame = new JFrame();
         frame.add(infobar, BorderLayout.SOUTH);
     }
 
-    //----------------------------windowsettings-------------------------------
     private void windowSettings(){
         frame.setTitle("Drone Simulation");
         frame.setLocation(100,100);
@@ -156,58 +136,22 @@ JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setExtendedState(frame.getExtendedState() | Frame.ICONIFIED); // minimize window
         frame.setBackground(mainScreenColor);
-        frame.setMinimumSize(new Dimension(1024,576));
+        frame.setMinimumSize(new Dimension(1440,810));
 
         frame.addComponentListener(new ComponentAdapter() {
             //----------------------------code for resizing the window-------------------------------
             @Override
             public void componentResized(ComponentEvent e) {
-                if(width!=frame.getWidth()) {
-                    int newWidth = frame.getWidth();
-                    int newHeight = (int) (frame.getWidth() / aspectRatio);
-                    Dimension newSize = new Dimension(newWidth,newHeight);
-                    frame.setSize(newSize);
-                }
-                else if(height!=frame.getHeight()) {
-                    int newHeight = frame.getHeight();
-                    int newWidth = (int) (newHeight * aspectRatio);
-                    Dimension newSize = new Dimension(newWidth,newHeight);
-                    frame.setSize(newSize);
-                }
-                else{
-                    int newWidth = frame.getWidth();
-                    int newHeight = frame.getHeight();
-                    Dimension newSize = new Dimension(newWidth,newHeight);
-                    frame.setSize(newSize);
-                }
-                width = frame.getHeight();
-                height = frame.getWidth();
-
-//----------------------------code for resizing components-------------------------------
-/*
-navbar.setPreferredSize(new Dimension(navbarSize.width,Math.max((navbarSize.height),30)));
-navbarButtonSize.height = Math.max(navbarButtonSize.height,20);
-tab1.setPreferredSize(navbarButtonSize);
-Dimension size = getSize();
-int newSize = (int) (size.width / 50);
-newSize = Math.max(newSize, 12);
-b1.setFont(new Font(b1.getFont().getName(), Font.PLAIN, newSize));
- */
+            width = frame.getWidth();
+            height = frame.getHeight();
+            card1.setPreferredSize(new Dimension(width,height*(1600/width)));
             }
         });
-    }
-
-
-
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
     }
 
 
-    //quickSet is a function that quickly changes Font, Fontcolor and Backgroundcolor of the given components
-    //it's used to change either of these properties for alot of components in one line
+    //function to set font, font color and background color
     protected static void quickSet(Font font, Color color1, Color color2, JComponent... components){
         for (JComponent component : components) {
             component.setFont(font);
