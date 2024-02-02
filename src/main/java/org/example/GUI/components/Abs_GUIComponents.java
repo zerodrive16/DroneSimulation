49d469f9ca85079cbdff006aebe69d41be191c1f
Stefan <110019272+zerodrive16@users.noreply.github.com>
2024-02-02 15:07:08 +0100

    Small changes

--- src/main/java/org/example/GUI/components/Abs_GUIComponents.java
+++ src/main/java/org/example/GUI/components/Abs_GUIComponents.java
@@ -1,7 +1,10 @@
 package org.example.GUI.components;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
+import java.util.Objects;
 
 /**
  * This abstract class sets the color and font for InfoBar, MainScreen, NavBar and the frame.
